949052f6579a515dfcfbd4cf0726fc6c22abee72
katrinapoulin <katrina.poulin@mail.mcgill.ca>
2022-03-19 10:02:59 -0400

    get_states

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,7 +1,19 @@
 # Student agent: Add your own agent here
 from agents.agent import Agent
 from store import register_agent
+import numpy as np
 import sys
+
+moves = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+max_steps = 0;
+
+
+class GameState():
+    def __init__(self, chess_board, p1_pos, p2_pos):
+        self.chess_board = chess_board
+        self.p1_pos = p1_pos
+        self.p2_pos = p2_pos
+        self.next_states = []
 
 
 @register_agent("student_agent")
@@ -36,5 +48,36 @@ class StudentAgent(Agent):
 
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
+        global max_steps
+        max_steps = max_step
+        states = get_states(chess_board, my_pos, adv_pos, max_step)
+        print(len(states[1]))
         # dummy return
         return my_pos, self.dir_map["u"]
+
+def get_states(chess_board, start_pos, adv_pos, max_step):
+    # BFS
+    state_queue = [(start_pos, 0)]
+    visited = {tuple(start_pos)}
+    walled_states = []
+    while state_queue:
+        cur_pos, cur_step = state_queue.pop(0)
+        r, c = cur_pos
+        if cur_step == max_step:
+            break
+        for dir, move in enumerate(moves):
+            if chess_board[r, c, dir]:
+                continue
+            
+            next_pos = (cur_pos[0] + move[0], cur_pos[1] + move[1])
+            if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
+                continue
+
+            visited.add(tuple(next_pos))
+            for d in range(4):
+                if not chess_board[r, c, d]:
+                    walled_states.append({next_pos, d})
+            state_queue.append((next_pos, cur_step + 1))
+
+    return walled_states
+
